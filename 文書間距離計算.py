9d3345e98180d1340da19702ceb75d80d97b4521
tadakazu1972 <tadakazu1972@gmail.com>
2019-01-09 21:59:27 +0900

    dist_norm

--- 文書間距離計算.py
+++ 文書間距離計算.py
@@ -21,7 +21,7 @@ print("#Files: %d, #features: %d" % (num_files, num_features))
 print(vectorizer.get_feature_names())
 
 #検索データ
-new_data = "大阪 府 北部 地震 による 災害 の 被害 を 受け た の です が 、 税金 の 減免 は 適用 さ れ ます か ？ A . ご 回答 内容 大阪 府 北部 を 震源 と する 地震 で 被災 さ れ た 方 について は 、 申請 により 、 市 税 の 減免 など が 受け られる 場合 が あり ます 。 詳細 は リンク 先 の 『 大阪 府 北部 を 震源 と する 地震 で 被災 さ れ た 方 に対する 市 税 の 減免 など について 別 ウィンドウ で 表示 し ます 』 を 参照 し て ください 。 【 対象 と なる 市 税 】"
+new_data = "予防　接種"
 new_data_vec = vectorizer.transform([new_data])
 
 #内容表示
@@ -33,6 +33,13 @@ def dist_raw(v1, v2):
     delta = v1 - v2
     return sp.linalg.norm(delta.toarray())
 
+#単語の出現回数ベクトルを正規化
+def dist_norm(v1, v2):
+    v1_normalized = v1/sp.linalg.norm(v1.toarray())
+    v2_normalized = v2/sp.linalg.norm(v2.toarray())
+    delta = v1_normalized - v2_normalized
+    return sp.linalg.norm(delta.toarray())
+
 #読み込んだファイル全て計算
 best_doc = None
 best_dist = sys.maxsize
@@ -42,7 +49,8 @@ for i in range(0, num_files):
     if temp_data == new_data:
         continue
     temp_data_vec = X_train.getrow(i)
-    d = dist_raw(temp_data_vec, new_data_vec)
+    #d = dist_raw(temp_data_vec, new_data_vec)
+    d = dist_norm(temp_data_vec, new_data_vec)
     print("=== Post %i with dist=%.2f: %s" % (i, d, temp_data))
     if d < best_dist:
         best_dist = d
